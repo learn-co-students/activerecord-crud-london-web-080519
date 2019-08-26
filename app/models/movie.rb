bd534d494436e7c7672cba8a1e669dc3803c52f9
george-kirby <georgeskirby@btinternet.com>
2019-08-26 17:54:02 +0100

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+
+    # attr_accessor :title, :release_date, :director, :lead, :in_theaters
+
+    # def initialize(attributes={})
+    #     @title = attributes[:title]
+    #     @release_date = attributes[:release_date]
+    #     @director = attributes[:director]
+    #     @lead = attributes[:lead]
+    #     @in_theaters = attributes[:in_theaters]
+    # end
 end
